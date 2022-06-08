e50efe0fc4498cad261eba8fc7b6869162175dc0
jjm6564 <79852201+jjm6564@users.noreply.github.com>
2022-06-09 00:41:30 +0900

    delete keystore info

--- klatynIDE/klaytninfo.py
+++ klatynIDE/klaytninfo.py
@@ -5,6 +5,6 @@ scriptPath = r'C:\Users\mvr\Desktop\JM\NFT\klatynIDE'
 scriptName = 'klaytn.sol'
 
 keyStorePath=r'C:\Users\mvr\Desktop\JM\NFT\keyStore'
-keyStoreName = 'kaikas-0x4c58228918f5639a62f10100b8e1f896afbd56bc (2)'
+keyStoreName = ''
 
 NFTName = 'lab715'
